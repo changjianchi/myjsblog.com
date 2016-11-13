bc32be1bb1b5a311e2b3afe871b56c3dc68377a4
changjianchi <changjianchi@qq.com>
2016-11-13 22:54:49 +0800

    s

--- script/list.js
+++ script/list.js
@@ -10,5 +10,5 @@ var list = [
     {"title": "click点击方法", "tip": "案例","link": "list/click/index.html"},
     {"title": "localstorage缓存", "tip": "案例","link": "list/localstorage/index.html"},
     {"title": "input拖动事件", "tip": "案例","link": "list/inputrange/index.html"},
-    {"title": "iscroll", "tip": "案例","link": "list/iscroll/index.html"}
+    {"title": "iscroll", "tip": "案例2","link": "list/iscroll/index.html"}
 ]
