858f4180c11f1afa3acc303b5b3a38f98106043c
changjianchi <changjianchi@qq.com>
2017-01-06 23:57:12 +0800

    案例更新

--- script/list.js
+++ script/list.js
@@ -14,5 +14,5 @@ var list = [
     {"title": "江苏艺术", "tip": "项目","link": "list/jsys/index.html"},
     {"title": "js实现下拉", "tip": "案例","link": "list/select/index.html"},
     {"title": "下拉显示元素", "tip": "案例","link": "list/xialaxianshiyuansu/index.html"},
-    {"title": "汇伯", "tip": "项目","link": "list/huibo/about.html"}
+    {"title": "幻灯片", "tip": "案例","link": "list/huandengpian/index.html"}
 ]
