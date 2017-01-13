28989d0bc6ba0335f02275290f0aa97374b8f282
changjianchi <changjianchi@qq.com>
2017-01-13 14:24:13 +0800

    添加统计

---
+++ script/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bbcb9a5eadae768b9dd536f3c2f5d936";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
