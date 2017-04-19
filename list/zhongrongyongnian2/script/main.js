80ec33c2f568475e0847187397bd32e12000da79
changjianchi <changjianchi@qq.com>
2017-04-19 16:16:36 +0800

    s

--- list/zhongrongyongnian2/script/main.js
+++ list/zhongrongyongnian2/script/main.js
@@ -87,6 +87,61 @@ $(function () {
             marginTop();
         }
 
+        var setSelect = function () {
+            var $select = $('.select_wrap:visible');
+            $.each($select, function (key, val) {
+                var $this = $(val).find('.select');
+                var flag = false;
+                var $valueHeight = $(val).find('.value').height();
+                var $height = $(val).find('.option').height();
+
+                $(val).css('zIndex', $select.length - key);
+
+                $this.find('.value').on('click', function () {
+                    if (flag) {
+                        $this.animate({
+                            height: $valueHeight + 'px'
+                        }, 50);
+                        flag = false;
+                    }
+                    else {
+                        $this.animate({
+                            height: ($height + $valueHeight) + 'px'
+                        }, 100);
+                        flag = true;
+
+                        setTimeout(function () {
+                            $(document).one('click', function () {
+                                $this.animate({
+                                    height: $valueHeight + 'px'
+                                }, 50);
+                                flag = false;
+                            });
+                        });
+                    }
+                });
+
+                $this.find('.option p').on('click mouseenter mouseleave', function (event) {
+                    if (event.type === 'click') {
+                        $this.find('.value').html($(this).text());
+                        $(this).addClass('active').siblings().removeClass('active');
+                        $this.animate({
+                            height: $valueHeight + 'px'
+                        }, 50);
+                        flag = false;
+                    }
+                    else if (event.type === 'mouseenter') {
+                        $(this).addClass('hover');
+                    }
+                    else {
+                        $(this).removeClass('hover');
+                    }
+                    
+                });
+
+            });
+        };
+
         $loginBtn.on('click', function () {
             if (flag) {
                 hide();
@@ -94,6 +149,7 @@ $(function () {
             else {
                 show();
                 flag = true;
+                setSelect();
                 marginTop();
             }
             return false;
@@ -155,65 +211,6 @@ $(function () {
         function () {
             $(this).find('.file_btn').removeClass('file_btn_active');
         });
-
-
-
-        var setSelect = function () {
-            var $select = $('.select_wrap:visible');
-            $.each($select, function (key, val) {
-                var $this = $(val).find('.select');
-                var flag = false;
-                var $valueHeight = $(val).find('.value').height();
-                var $height = $(val).find('.option').height();
-
-                $(val).css('zIndex', $select.length - key);
-
-                $this.find('.value').on('click', function () {
-                    if (flag) {
-                        $this.animate({
-                            height: $valueHeight + 'px'
-                        }, 50);
-                        flag = false;
-                    }
-                    else {
-                        $this.animate({
-                            height: ($height + $valueHeight) + 'px'
-                        }, 100);
-                        flag = true;
-
-                        setTimeout(function () {
-                            $(document).one('click', function () {
-                                $this.animate({
-                                    height: $valueHeight + 'px'
-                                }, 50);
-                                flag = false;
-                            });
-                        });
-                    }
-                });
-
-                $this.find('.option p').on('click mouseenter mouseleave', function (event) {
-                    if (event.type === 'click') {
-                        $this.find('.value').html($(this).text());
-                        $(this).addClass('active').siblings().removeClass('active');
-                        $this.animate({
-                            height: $valueHeight + 'px'
-                        }, 50);
-                        flag = false;
-                    }
-                    else if (event.type === 'mouseenter') {
-                        $(this).addClass('hover');
-                    }
-                    else {
-                        $(this).removeClass('hover');
-                    }
-                    
-                });
-
-            });
-        };
-        
-        setSelect();
     })(jQuery);
 
     (function($) {
