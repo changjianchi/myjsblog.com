2bd4b65c72e7051d112c3ad75be1e2389ef3807a
changjianchi <changjianchi@qq.com>
2017-08-16 01:23:46 +0800

    s

--- public/list/zllx/script/main.js
+++ public/list/zllx/script/main.js
@@ -24,6 +24,20 @@ $(function () {
         });
     });
 
+    var $theme = $('.qqdr_ban_info .qqdr_ul li');
+    $.each($theme, function (key, val) {
+        var $this = $(val);
+        $(val).hover(function () {
+            $this.find('.bg').animate({
+                'opacity': 0
+            }, 100);
+        }, function () {
+            $this.find('.bg').stop(true).animate({
+                'opacity': .6
+            }, 100);
+        });
+    });
+
     var $list_box = $container.find('.list_box');
     $list_box.find('input, textarea').on('focus blur input change', function (event) {
         if (event.type === 'focus') {
@@ -35,11 +49,17 @@ $(function () {
         else {
             if ($(this).val() === '') {
                 $(this).parent().find('.bg2').show();
+                $(this).parent().find('.bg3').find('em').show();
             }
             else {
                 $(this).parent().find('.bg2').hide();
+                $(this).parent().find('.bg3').find('em').hide();
             }
         }
+    });
+
+    $list_box.on('click', function () {
+        $(this).find('input').focus();
     });
 
     $('.ewm').hover(function () {
