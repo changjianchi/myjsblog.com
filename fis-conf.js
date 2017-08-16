10b5fc3be08fa05d199afd804107a43a67fd65ef
changjianchi <changjianchi@qq.com>
2017-08-16 10:27:24 +0800

    s

--- fis-conf.js
+++ fis-conf.js
@@ -8,7 +8,7 @@ function push(receiver, to) {
     });
 }
 
-var dev = 'http://www.febaidu.com:30000/receiver';
+var dev = 'http://115.28.207.231:30000/';
 
 fis.match('*', {
     deploy: push(dev, '/home/work/wwwroot/myjsblog.com')
